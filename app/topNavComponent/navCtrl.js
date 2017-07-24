f05b5e507d0aa48c2cfb62f484e2a4cb64f915e7
Mauricio Saavedra <masavi2008@hotmail.com>
2017-07-24 17:57:24 -0500

    Add AngularJS & Navbar Component

---
+++ app/topNavComponent/navCtrl.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('myApp')
+    .component('navComponent', {
+      templateUrl: '/app/topNavComponent/topNav.html',
+      controller: navCtrl,
+      controllerAs: 'vm'
+  });
+
+  function navCtrl() {
+      var vm = this;
+      $('.modal').modal();
+  }
+
+})();
