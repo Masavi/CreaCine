202584248199a905f0662fdcd9135b548a68fd9f
Mauricio Saavedra <masavi2008@hotmail.com>
2017-07-26 23:43:28 -0500

    Add Materialize CSS
    Changing from Bulma to Materialize CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,14 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <nav className="navbar">
-          <div className="navbar-brand">
-
-            <a className="navbar-item" href="http://bulma.io">
-              <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
-            </a>
-
-            <div class="navbar-burger">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            
+        <nav>
+          <div className="nav-wrapper">
+            <a href="" className="brand-logo">Logo</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
           </div>
         </nav>
       </div>
